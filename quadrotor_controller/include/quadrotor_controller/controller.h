1bf10daa0401ecdc4885b64b583313d604cf66b5
1moule <1961466188@qq.com>
2025-09-24 10:33:41 +0800

    Add ocs2 relative func.

--- quadrotor_controller/include/quadrotor_controller/controller.h
+++ quadrotor_controller/include/quadrotor_controller/controller.h
@@ -5,7 +5,13 @@
 #pragma once
 
 #include <controller_interface/multi_interface_controller.h>
+#include <ocs2_core/thread_support/ExecuteAndSleep.h>
+#include <ocs2_core/thread_support/SetThreadPriority.h>
+#include <ocs2_ddp/GaussNewtonDDP_MPC.h>
+#include <ocs2_mpc/MPC_MRT_Interface.h>
+#include <ocs2_msgs/mpc_observation.h>
 #include <quadrotor_common/wrench_interface.h>
+#include <quadrotor_interface/QuadrotorInterface.h>
 
 namespace quadrotor_controller
 {
@@ -18,9 +24,31 @@ public:
   bool init(
     hardware_interface::RobotHW * robot_hw, ros::NodeHandle & root_nh,
     ros::NodeHandle & controller_nh) override;
+  void starting(const ros::Time & time) override;
   void update(const ros::Time & time, const ros::Duration & period) override;
 
 private:
+  void setupMpc(ros::NodeHandle & nh);
+  void setupMrt();
+  void updateEstimation(const ros::Time & time, const ros::Duration period);
+
   hardware_interface::QuadrotorWrenchHandle wrench_handle_;
+
+  //mpc
+  // Interface
+  std::shared_ptr<quadrotor_interface::QuadrotorInterface> quadrotor_interface_;
+
+  // State Estimation
+  ocs2::SystemObservation currentObservation_;
+
+  // MPC
+  std::shared_ptr<ocs2::MPC_BASE> mpc_;
+  std::shared_ptr<ocs2::MPC_MRT_Interface> mpcMrtInterface_;
+
+  std::thread mpcThread_;
+  std::atomic_bool controllerRunning_{}, mpcRunning_{};
+  ocs2::benchmark::RepeatedTimer mpcTimer_;
+
+  ros::Publisher observationPublisher_;
 };
 }  // namespace quadrotor_controller
--- quadrotor_controller/src/controller.cpp
+++ quadrotor_controller/src/controller.cpp
@@ -15,21 +15,122 @@ bool QuadrotorController::init(
   // Hardware interface
   auto * wrenchInterface = robot_hw->get<hardware_interface::QuadrotorWrenchInterface>();
   wrench_handle_ = wrenchInterface->getHandle("wrench");
+
+  // Initialize OCS2
+  std::string taskFile;
+  std::string libFolder;
+  controller_nh.getParam("/taskFile", taskFile);
+  controller_nh.getParam("/libFolder", libFolder);
+  quadrotor_interface_ =
+    std::make_shared<quadrotor_interface::QuadrotorInterface>(taskFile, libFolder);
+  setupMpc(controller_nh);
+  setupMrt();
+
   return true;
+}
+
+void QuadrotorController::starting(const ros::Time & time)
+{
+  // Initial state
+  currentObservation_.state.setZero(ocs2::quadrotor::STATE_DIM);
+  updateEstimation(time, ros::Duration(0.001));
+  currentObservation_.input.setZero(ocs2::quadrotor::INPUT_DIM);
+
+  ocs2::TargetTrajectories target_trajectories(
+    {currentObservation_.time}, {currentObservation_.state}, {currentObservation_.input});
+
+  // Set the first observation and command and wait for optimization to finish
+  mpcMrtInterface_->setCurrentObservation(currentObservation_);
+  mpcMrtInterface_->getReferenceManager().setTargetTrajectories(target_trajectories);
+  ROS_INFO_STREAM("Waiting for the initial policy ...");
+  while (!mpcMrtInterface_->initialPolicyReceived() && ros::ok()) {
+    mpcMrtInterface_->advanceMpc();
+    ros::WallRate(quadrotor_interface_->mpcSettings().mrtDesiredFrequency_).sleep();
+  }
+  ROS_INFO_STREAM("Initial policy has been received.");
+
+  mpcRunning_ = true;
+}
+
+void QuadrotorController::updateEstimation(const ros::Time & time, const ros::Duration period)
+{
+  //TODO:Update the state estimation here
+  mpcMrtInterface_->setCurrentObservation(currentObservation_);
 }
 
 void QuadrotorController::update(const ros::Time & time, const ros::Duration & period)
 {
+  // State Estimate
+  updateEstimation(time, period);
+
+  // Update the current state of the system
+  mpcMrtInterface_->setCurrentObservation(currentObservation_);
+
+  // Load the latest MPC policy
+  mpcMrtInterface_->updatePolicy();
+
+  // Evaluate the current policy
+  ocs2::vector_t optimizedState, optimizedInput;
+  size_t plannedMode = 0;  // The mode that is active at the time the policy is evaluated at.
+  mpcMrtInterface_->evaluatePolicy(
+    currentObservation_.time, currentObservation_.state, optimizedState, optimizedInput,
+    plannedMode);
+
   geometry_msgs::Wrench cmd;
   cmd.force.x = 0;
   cmd.force.y = 0;
-  cmd.force.z = 15;
-  cmd.torque.x = 0;
-  cmd.torque.y = 0;
-  cmd.torque.z = 0;
+  cmd.force.z = optimizedInput[0];
+  cmd.torque.x = optimizedInput[1];
+  cmd.torque.y = optimizedInput[2];
+  cmd.torque.z = optimizedInput[3];
   wrench_handle_.setCommand(cmd);
 }
 
+void QuadrotorController::setupMpc(ros::NodeHandle & nh)
+{
+  const std::string robotName = "quadrotor";
+  mpc_ = std::make_shared<ocs2::GaussNewtonDDP_MPC>(
+    quadrotor_interface_->mpcSettings(), quadrotor_interface_->ddpSettings(),
+    quadrotor_interface_->getRollout(), quadrotor_interface_->getOptimalControlProblem(),
+    quadrotor_interface_->getInitializer());
+  auto rosReferenceManagerPtr = std::make_shared<quadrotor_interface::RosReferenceManager>(
+    quadrotor_interface_->getReferenceManagerPtr());
+  rosReferenceManagerPtr->subscribe(nh);
+
+  mpc_->getSolverPtr()->setReferenceManager(rosReferenceManagerPtr);
+
+  observationPublisher_ =
+    nh.advertise<ocs2_msgs::mpc_observation>(robotName + "_mpc_observation", 1);
+}
+
+void QuadrotorController::setupMrt()
+{
+  mpcMrtInterface_ = std::make_shared<ocs2::MPC_MRT_Interface>(*mpc_);
+  mpcMrtInterface_->initRollout(&quadrotor_interface_->getRollout());
+  mpcTimer_.reset();
+
+  controllerRunning_ = true;
+  mpcThread_ = std::thread([&]() {
+    while (controllerRunning_) {
+      try {
+        ocs2::executeAndSleep(
+          [&]() {
+            if (mpcRunning_) {
+              mpcTimer_.startTimer();
+              mpcMrtInterface_->advanceMpc();
+              mpcTimer_.endTimer();
+            }
+          },
+          quadrotor_interface_->mpcSettings().mpcDesiredFrequency_);
+      } catch (const std::exception & e) {
+        controllerRunning_ = false;
+        ROS_ERROR_STREAM("[Ocs2 MPC thread] Error : " << e.what());
+        stopRequest(ros::Time());
+      }
+    }
+  });
+  ocs2::setThreadPriority(50, mpcThread_);
+}
 }  // namespace quadrotor_controller
 PLUGINLIB_EXPORT_CLASS(
   quadrotor_controller::QuadrotorController, controller_interface::ControllerBase)
